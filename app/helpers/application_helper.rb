c1857404cbb424f33789a71dc6ba7f8ab82be809
vagrant <vagrant@vagrant.(none)>
2013-07-24 22:57:39 +0000

    GENERATE CODES HELPER

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+	def random_codes
+		string = []
+		o =  [('a'..'z'),(0..9)].map{|i| i.to_a}.flatten
+
+		# loop to create
+		i = 0
+		loop do
+			string << (0...8).map{ o[rand(o.length)] }.join
+			i += 1
+			break if i == 800000
+		end
+
+		string.sort
+	end
+
 end
