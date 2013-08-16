aa7943049286987997781649cacf5e8750abf629
vagrant <vagrant@vagrant.(none)>
2013-08-16 12:00:44 +0000

    All clear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  	  flash[:success] = 'Welcome!'
+  	  flash[:success] = 'Успех!'
   	  redirect_to @user
   	else
   	  render 'new'
